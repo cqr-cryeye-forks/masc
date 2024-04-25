3ec96dc00af528353f8fa90ff1f614999fe4c526
prokann02 <prokopovann02@gmail.com>
2024-04-25 15:26:09 +0300

    Changed name

--- load_links.py
+++ load_links.py
@@ -9,17 +9,24 @@ def load_links():
     with open("output.txt", 'r') as f:
         links = f.readlines()
 
+    counter = 0
     for link in links:
+        link = link.strip()
         if not link.startswith('http'):
             continue
 
         resource_response = requests.get(link, verify=False)
         print(link)
 
-        filename = os.path.join(PATH_TO_CONTENT_FOLDER, os.path.basename(link))
+        name = link.split('/')[-1]
+        if not name:
+            name = str(counter)
+        filename = os.path.join(PATH_TO_CONTENT_FOLDER, name)
         if resource_response.status_code == 200:
             with open(filename, 'wb') as f:
                 f.write(resource_response.content)
             print(f"Resource {filename} downloaded.")
         else:
             print(f"Resource {filename} not downloaded: {resource_response.status_code}")
+
+        counter += 1
